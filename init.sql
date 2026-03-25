4ee8e2004ab0d8232df67461d3b80054c67e0fb0
Theo Fischer <admin@thefischer.org>
2026-03-25 12:37:21 +0100

    Refactor table definitions in init.sql for consistency and clarity

--- init.sql
+++ init.sql
@@ -1,21 +1,32 @@
 CREATE TABLE users (
-    "ID" serial PRIMARY KEY,
-    "Firstname" character varying(32) NOT NULL,
-    "Surname" character varying(32) NOT NULL,
-    "Date of birth" date NOT NULL,
-    "Gender" smallint DEFAULT 0 NOT NULL CHECK ("Gender" IN (0, 1, 2)),
-    "Body weight" real NOT NULL,
-    "Body height" real NOT NULL,
-    "Drag area" real NOT NULL,
-    "Drag coefficient" real NOT NULL
+    "id" serial PRIMARY KEY,
+    "first_name" character varying(32) NOT NULL,
+    "last_name" character varying(32) NOT NULL,
+    "date_of_birth" date NOT NULL,
+    "gender" smallint DEFAULT 0 CHECK ("gender" IN (0, 1, 2)),
+    "body_weight_kg" real NOT NULL,
+    "body_height_m" real NOT NULL,
+    "drag_area_m2" real NOT NULL,
+    "drag_coefficient" real NOT NULL
 );
 
 CREATE TABLE bicycles (
-    "ID" serial PRIMARY KEY,
-    "Label" character varying(64) NOT NULL,
-    "Wheel size" real DEFAULT 0.68 NOT NULL,
-    "Crank length" real NOT NULL,
-    "Weight" real DEFAULT 6.8 NOT NULL,
-    "Chainring size" smallint NOT NULL,
-    "Sprocket size" smallint DEFAULT 12 NOT NULL
+    "id" serial PRIMARY KEY,
+    "label" character varying(64) NOT NULL,
+    "wheel_size_m" real DEFAULT 0.68,
+    "crank_length_m" real NOT NULL,
+    "weight_kg" real DEFAULT 6.8,
+    "chainring_size" smallint NOT NULL,
+    "sprocket_size" smallint DEFAULT 12
+);
+
+CREATE TABLE workouts (
+    "id" serial PRIMARY KEY,
+    "user_id" integer NOT NULL REFERENCES users("id") ON DELETE CASCADE,
+    "bicycle_id" integer NOT NULL REFERENCES bicycles("id") ON DELETE CASCADE,
+    "date" date NOT NULL,
+    "duration_s" int NOT NULL,
+    "distance_km" real NOT NULL,
+    "average_speed_kmh" real NOT NULL,
+    "average_power_w" real NOT NULL
 );
